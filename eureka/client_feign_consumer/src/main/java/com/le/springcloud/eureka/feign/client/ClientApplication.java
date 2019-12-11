9af23979eb5f681f94c08f828e5e4eb59f329642
黄佳乐 <huangjiale@idwzx.com>
2019-12-11 13:10:25 +0800

    Hystrix监控页面添加

--- eureka/client_feign_consumer/src/main/java/com/le/springcloud/eureka/feign/client/ClientApplication.java
+++ eureka/client_feign_consumer/src/main/java/com/le/springcloud/eureka/feign/client/ClientApplication.java
@@ -2,7 +2,8 @@ package com.le.springcloud.eureka.feign.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,9 +11,12 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date ：2019/9/26 14:49
  * @description： 启动类
  * @modified By：
+ * EnableHystrixDashboard 可以单独启动一个工程
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableHystrix
+@EnableHystrixDashboard
 public class ClientApplication {
 
     public static void main(String[] args) {
